463f140ca92fb1ac744dc689ad422a1f7c619716
Filippo Gasparini <filippogaspa02@gmail.com>
2024-11-05 15:06:37 +0100

    ADD: API

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log('API')
+
+
+console.log(axios)
+
+
+axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+.then ((res) => {
+    console.log('Risposta confermata', res)
+// qui abbiamo la risposta del server
+})
+.catch ((err) => {
+    console.log('Risposta errata', err)
+// quando esce questo avviso, vuol dire che qualcosa non va 
+})
