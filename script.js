583a15a91d7fc866f0ea9d72a6153aa34d4e0a9b
Filippo Gasparini <filippogaspa02@gmail.com>
2024-11-05 16:11:01 +0100

    ADD: genero le mail

--- script.js
+++ script.js
@@ -4,12 +4,36 @@ console.log('API')
 console.log(axios)
 
 
-axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-.then ((res) => {
-    console.log('Risposta confermata', res)
+//axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+//.then ((res) => {
 // qui abbiamo la risposta del server
-})
-.catch ((err) => {
-    console.log('Risposta errata', err)
+  //  console.log('Risposta confermata', res)
+    //const mail = res.data.response
+  //  console.log(mail)
+    
+//})
+
+//.catch ((err) => {
+  //  console.log('Risposta errata', err)
 // quando esce questo avviso, vuol dire che qualcosa non va 
-})
+//})
+
+
+function getRandomEmails(n) {
+    const url = "https://flynn.boolean.careers/exercises/api/random/mail";
+    const emails = [];
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
